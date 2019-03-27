3a14229f6b4729078a1d72899e1ad68360652d00
Ravish Rajput <ravish.rajput@capgemini.com>
2019-03-27 14:29:11 +0530

    splash screen - added handler to navigate

--- app/src/main/java/com/example/demoapp/SplashActivity.java
+++ app/src/main/java/com/example/demoapp/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.demoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +11,27 @@ import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
